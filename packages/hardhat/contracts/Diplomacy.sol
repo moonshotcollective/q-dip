5d9658203733b1dc74514f0f6bd0c8efa186072f
qedk <1994constant@gmail.com>
2021-09-07 00:44:33 +0530

    Add meta-transaction support for QD contracts

--- packages/hardhat/contracts/Diplomacy.sol
+++ packages/hardhat/contracts/Diplomacy.sol
@@ -42,8 +42,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./EIP712MetaTransaction.sol";
 
-contract Diplomacy is AccessControl, Ownable, ReentrancyGuard {
+contract Diplomacy is AccessControl, Ownable, ReentrancyGuard, EIP712MetaTransaction("Diplomacy", "1") {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
