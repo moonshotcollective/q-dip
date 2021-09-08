5e8c8aee8d02b41642fba2e8738a353649c7890a
hans <hansmrtn@pm.me>
2021-09-08 04:46:07 +0000

    added: tests and gas reporter

--- packages/hardhat/test/DiplomacyTest.js
+++ packages/hardhat/test/DiplomacyTest.js
@@ -1,6 +1,7 @@
 const { ethers } = require("hardhat");
 const { use, expect } = require("chai");
 const { solidity } = require("ethereum-waffle");
+const { asciiToHex } = require("web3-utils");
 
 use(solidity);
 
@@ -10,7 +11,7 @@ describe("Quadratic Diplomacy", function () {
     it("Should deploy Diplomacy", async function () {
       const Diplomacy = await ethers.getContractFactory("Diplomacy");
       diplomacy = await Diplomacy.deploy();
-      console.log(diplomacy)
+      // console.log(diplomacy)
     });
 
     describe("newElection()", function () {
@@ -22,29 +23,29 @@ describe("Quadratic Diplomacy", function () {
           10, 
           [
             "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-            "0x01684C57AE8a4226271068210Ce1cCED865a5AfC",
-            "0xf5De4337Ac5332aF11BffbeC45D950bDDBc1493F",
-            "0x4E53E14de4e264AC2C3fF501ed3Bd6c4Ad63B9A1",
+            // "0x01684C57AE8a4226271068210Ce1cCED865a5AfC",
+            // "0xf5De4337Ac5332aF11BffbeC45D950bDDBc1493F",
+            // "0x4E53E14de4e264AC2C3fF501ed3Bd6c4Ad63B9A1",
           ]
         );
       });
     });
 
     describe("castBallot()", function () {
-      it("shoould be able to cast a ballot", async function () {
+      it("should be able to cast a ballot", async function () {
         await diplomacy.castBallot(
           0, 
           [
             "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-            "0x01684C57AE8a4226271068210Ce1cCED865a5AfC",
-            "0xf5De4337Ac5332aF11BffbeC45D950bDDBc1493F",
-            "0x4E53E14de4e264AC2C3fF501ed3Bd6c4Ad63B9A1",
+            // "0x01684C57AE8a4226271068210Ce1cCED865a5AfC",
+            // "0xf5De4337Ac5332aF11BffbeC45D950bDDBc1493F",
+            // "0x4E53E14de4e264AC2C3fF501ed3Bd6c4Ad63B9A1",
           ], 
           [
             "1", 
-            "2", 
-            "1", 
-            "0"
+            // "2", 
+            // "1", 
+            // "0"
           ]
         );
       });
@@ -52,9 +53,34 @@ describe("Quadratic Diplomacy", function () {
 
     describe("endElection()", function () {
       it("should be able to end an election", async function () {
-        0
+        await diplomacy.endElection(
+          0
+        )
       });
     });
+
+    describe("payoutElection()", function () {
+      it("should be able to payout an election", async function () {
+        await diplomacy.payoutElection(
+          0, 
+          [
+            "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+            // "0x01684C57AE8a4226271068210Ce1cCED865a5AfC",
+            // "0xf5De4337Ac5332aF11BffbeC45D950bDDBc1493F",
+            // "0x4E53E14de4e264AC2C3fF501ed3Bd6c4Ad63B9A1",
+          ],  
+          [
+            1, 
+            // "1", 
+            // "1", 
+            // "1"
+          ], 
+          {
+            value: 4
+          }
+        )
+      });
+    }); 
 
   });
 });
