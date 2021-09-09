6d4e885a79efe779ce548db097d5f31708839222
karbafoo <karbafooo@gmail.com>
2021-09-09 11:43:06 +0430

    Revert "spider chart  🕷️"
    This reverts commit 067761c72ae370a9cd23b528bcc14678c74a2a13.

--- packages/react-app/src/components/index.js
+++ packages/react-app/src/components/index.js
@@ -18,4 +18,3 @@ export { default as TokenBalance } from "./TokenBalance";
 export { default as Wallet } from "./Wallet";
 export { default as L2Bridge } from "./L2Bridge";
 export { default as PayButton } from "./PayButton";
-export { SpiderChart } from "./SpiderChart";
