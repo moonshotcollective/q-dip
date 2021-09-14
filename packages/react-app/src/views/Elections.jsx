09e316462358593e8ec01b1a657d83ba47d9d6ed
Hans <hansmrtn@pm.me>
2021-09-14 04:03:32 +0000

    added tags

--- packages/react-app/src/views/Elections.jsx
+++ packages/react-app/src/views/Elections.jsx
@@ -1,3 +1,8 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
+import { useEventListener } from "../hooks";
+import { Address } from "../components";
+import { mainnetProvider, blockExplorer } from "../App";
 import {
   Button,
   Divider,
@@ -15,9 +20,6 @@ import {
   Carousel,
   Typography,
 } from "antd";
-import React, { useState, useEffect, useRef } from "react";
-import { useHistory } from "react-router-dom";
-import { useEventListener } from "../hooks";
 
 var Map = require("collections/map");
 
@@ -41,15 +43,42 @@ export default function Elections({
       title: "Name",
       dataIndex: "name",
       key: "name",
-      width: 150,
       align: "center",
       render: name => <Typography.Title level={5}>{name}</Typography.Title>,
     },
     {
-      title: "Voted",
+      title: "Creator", 
+      dataIndex: "creator", 
+      key: "creator",
+      align: "center", 
+      render: creator => <Address address={creator} fontSize="14pt" ensProvider={mainnetProvider} blockExplorer={blockExplorer} />
+    },
+    {
+      title: "№ Voted",
       dataIndex: "n_voted",
       key: "n_voted",
       align: "center",
+    },
+    {
+      title: "Tags", 
+      dataIndex: "tags", 
+      key: "tags", 
+      align: "center", 
+      render: tags =>
+        tags.map(r => {
+          let color = "geekblue";
+          if (r == "candidate") {
+            color = "green";
+          } 
+          if ( r === "voted" ) {
+            color = "purple";
+          }
+          return (
+            <Tag color={color} key={r}>
+              {r.toLowerCase()}
+            </Tag>
+          );
+        }),
     },
     {
       title: "Action",
@@ -61,7 +90,7 @@ export default function Elections({
             <Button type="default" size="small" shape="round" onClick={() => viewElection(record)}>
               View
             </Button>
-            <Button type="default" size="small" shape="round" onClick={() => voteElection(record)}>
+            <Button type="default" size="small" shape="round" onClick={() => voteElection(index)}>
               Vote
             </Button>
           </Space>
@@ -87,15 +116,33 @@ export default function Elections({
   const init = async () => {
     
     const electionsMap = new Map();
-    
     const numElections = await readContracts.Diplomacy.numElections();
     for (let i = 0; i < numElections; i++) {
+  
       const election = await readContracts.Diplomacy.getElectionById(i);
-      const electionVoted = await readContracts.Diplomacy.getElectionVoted(i);
-      console.log({ electionVoted })
-      let electionEntry = {};
-      electionEntry.name = election.name;
-      electionEntry.n_voted = electionVoted.toNumber();
+      console.log({ election })
+      
+      const electionVoted = await readContracts.Diplomacy.getElectionVoted(i);  
+      const hasVoted = await readContracts.Diplomacy.hasVoted(i, address);
+
+      const tags = []
+      if ( election.admin === address ) {
+        tags.push("admin");
+      }      
+      if ( election.candidates.includes(address) ) {
+        tags.push("candidate");
+      }
+      if ( hasVoted ) {
+        tags.push("voted")
+      }
+
+      let electionEntry = {
+        name: election.name,
+        creator: election.admin,
+        n_voted: electionVoted.toNumber(),
+        tags: tags,
+      };
+
       electionsMap.set(i, electionEntry);
     }
 
@@ -116,14 +163,14 @@ export default function Elections({
     });
   };
 
-  function onBallotCast(msg, electionsMap) {
+  const onBallotCast = async (msg, electionsMap) => {
     console.log("onBallotCast ", msg);
     let election = electionsMap.get(msg.electionId.toNumber());
     election.n_voted = election.n_voted + 1;
     electionsMap.set(msg.electionId.toNumber(), election);
   }
 
-  async function onElectionCreated(msg, electionsMap) {
+  const onElectionCreated = async (msg, electionsMap) => {
     console.log("onElectionCreated ", msg);
     const election = await readContracts.Diplomacy.getElectionById(msg.electionId.toNumber());
     const electionVoted = await readContracts.Diplomacy.getElectionVoted(msg.electionId.toNumber());
@@ -133,10 +180,10 @@ export default function Elections({
     electionsMap.set(msg, electionEntry);
   }
 
-  function voteElection(record) {
+  function voteElection(index) {
     let adrs = ["0x7F2FA234AEd9F7FA0D5070Fb325D1c2C983E96b1", "0x154e80Ebc2e4769A1B680CAC800eE3A2613dC8D6"];
     let votes = [2, 3];
-    const result = tx(writeContracts.Diplomacy.castBallot(0, adrs, votes), update => {
+    const result = tx(writeContracts.Diplomacy.castBallot(index, adrs, votes), update => {
       console.log("📡 Transaction Update:", update);
     });
   }
@@ -147,7 +194,17 @@ export default function Elections({
         className="elections-view"
         style={{ border: "1px solid #cccccc", padding: 16, width: 1000, margin: "auto", marginTop: 64 }}
       >
-        {electionsMap && <Table dataSource={Array.from(electionsMap.values())} columns={columns}></Table>}
+        <PageHeader
+          ghost={false}
+          title="Elections"
+          extra={[
+            <Button type="primary" size="large" shape="round" style={{ margin: 4 }} onClick={() => createNewElection()}>
+              + Create Election
+            </Button>,
+          ]}
+        />
+        <Divider />
+        {electionsMap && <Table dataSource={Array.from(electionsMap.values())} columns={columns} pagination={false}></Table>}
       </div>
     </>
   );
